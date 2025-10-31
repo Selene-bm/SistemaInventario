693c587d78d434c160b260c985af3f29cfa24ff3
SSamuelP <samuelsantiagossp10@gmail.com>
2025-10-31 11:13:43 -0500

    Creación del model de usuario

--- cuenta_app/models.py
+++ cuenta_app/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class Usuario(AbstractUser):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    rol = models.CharField(max_length=50, choices=[
+        ('Administrador', 'Administrador'),
+        ('Supervisor', 'Supervisor'),
+        ('Empleado', 'Empleado')])
+
+    class Meta:
+        db_table = "Usuarios"
+
+    def __str__(self):
+        return f"Usuario: {self.nombre} {self.apellido}, Correo: {self.email}, Rol: {self.rol}"
